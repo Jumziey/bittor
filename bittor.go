0c70fb27fd47f0685122413c12f0bfe24b880773
Jesper Vesterberg <jumziey@gmail.com>
2014-03-11 18:03:52 +0100

    Seriously... a year ago i must have smoked allot of pot. This is some really weird code occasionally xD But well, now you only need to pass a []byte slice with bencoded data instead of a file name which the library opens! (i REALLY didn't like that)

--- bittor.go
+++ bittor.go
@@ -1,63 +1,61 @@
 package bittor
 
-import(
-	"strconv"
+import (
 	"errors"
 	"fmt"
-	"io/ioutil"
 	"reflect"
+	"strconv"
 )
-
 
 type torData struct {
 	data []byte
-	pos int
-	
-	//Gives the name of the torrent thats parsed, used for error messages. 
-	tfile string
+	pos  int
+
+	//Gives the name of the torrent thats parsed, used for error messages.
+	name string
 }
 
-func (t *torData)next() byte {
+func (t *torData) next() byte {
 	b := t.data[t.pos]
-	t.pos = t.pos+1
+	t.pos = t.pos + 1
 	return b
 }
 
-func (t *torData)peek() byte {
+func (t *torData) peek() byte {
 	return t.data[t.pos]
 }
 
-func (t *torData)prev() {
-	t.pos = t.pos-1
+func (t *torData) prev() {
+	t.pos = t.pos - 1
 }
 
 func intParse(t *torData) (int, error) {
 	intStr := ""
 	var b byte
-	for b= t.next(); b != 'e'; b = t.next() {
-		intStr = intStr+string(b)
+	for b = t.next(); b != 'e'; b = t.next() {
+		intStr = intStr + string(b)
 	}
 	integ, err := strconv.Atoi(intStr)
 	if err != nil {
-		return 0, errors.New(fmt.Sprint("Error in intParse at ", t.pos, ", in: ",t.tfile))
+		return 0, errors.New(fmt.Sprint("Error in intParse at ", t.pos, ", in: ", t.name))
 	}
 	return integ, nil
 }
 
 func stringParse(t *torData) (string, error) {
 	t.prev()
-	
+
 	stringSize := ""
-	for s:=t.next(); s != ':'; s=t.next() {
-		stringSize = stringSize+string(s)
+	for s := t.next(); s != ':'; s = t.next() {
+		stringSize = stringSize + string(s)
 	}
 	s_size, err := strconv.Atoi(stringSize)
 	if err != nil {
-		return "", errors.New(fmt.Sprint("Error in stringParse at ", t.pos, ", in: ",t.tfile))
+		return "", errors.New(fmt.Sprint("Error in stringParse at ", t.pos, ", in: ", t.name))
 	}
-	
+
 	bstring := make([]byte, s_size)
-	for i:=0; i<s_size; i++ {
+	for i := 0; i < s_size; i++ {
 		bstring[i] = t.next()
 	}
 	return string(bstring), nil
@@ -76,13 +74,13 @@ func listParse(t *torData) ([]interface{}, error) {
 		itemSlice = append(itemSlice, s)
 	}
 	t.next() //Throw away the 'e'
-	
+
 	return itemSlice, nil
 }
 
 func dictParse(t *torData) (map[string]interface{}, error) {
 	dictMap := make(map[string]interface{})
-	
+
 	//We read until we reach the end 'e' of the dictionary and make this
 	//a dictionary item. We peek so we don't fuck it up for nextItem(*Data).
 	//We must be able to read two items at a time, otherwise the torrent is faulty
@@ -92,52 +90,49 @@ func dictParse(t *torData) (map[string]interface{}, error) {
 		if err != nil {
 			return nil, errors.New(fmt.Sprint("Error in dictParse(): ", err))
 		}
-		
+
 		value, err := nextItem(t)
 		if err != nil {
 			return nil, errors.New(fmt.Sprint("Error in dictParse(): ", err))
 		}
-		
+
 		dictMap[key.(string)] = value
 	}
 	t.next() //Throw away the 'e'
-	
+
 	return dictMap, nil
 }
 
 func nextItem(t *torData) (interface{}, error) {
 	switch t.next() {
-		case 'd':
-			return dictParse(t)
-		case 'l':
-			return listParse(t)
-		case 'i':
-			return intParse(t)
-		case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
-			return stringParse(t)
-		default:
-			return nil, errors.New(fmt.Sprint("Out of bounds in nextItem() at ", t.pos," in: ", t.tfile, " (Probably badly encoded torrent)"))
+	case 'd':
+		return dictParse(t)
+	case 'l':
+		return listParse(t)
+	case 'i':
+		return intParse(t)
+	case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9':
+		return stringParse(t)
+	default:
+		return nil, errors.New(fmt.Sprint("Out of bounds in nextItem() at ", t.pos, " in: ", t.name, " (Probably badly encoded torrent)"))
 	}
 	//Unreachable, but needed due to weird controls in go-compiler
 	return nil, nil
 }
 
 //Reads in the torrentfile f_name and assumes that the torrent just have one
-//main dict item (altough the main dict can contain arbitrary many items). 
+//main dict item (altough the main dict can contain arbitrary many items).
 //The dict itself is assumed to only have strings or ints as keys (Altough both are
 //represented as strings) the value can be anything and is returned as an
-//interface{}, type is assertion needed to properly access the value. 
-func GetMainDict(f_name string) (map[string]interface{}, error) {
+//interface{}, type assertion is needed to properly access the value.
+func GetMainDict(tData []byte, tName string) (map[string]interface{}, error) {
 	var t torData
 	var err error
-	
-	t.data, err = ioutil.ReadFile(f_name)
-	t.tfile = f_name
-	if err != nil {
-		return nil, err
-	}
-	
-	mainDict, err:= nextItem(&t)
+
+	t.data = tData
+	t.name = tName
+
+	mainDict, err := nextItem(&t)
 	if err != nil {
 		return nil, err
 	}
@@ -148,50 +143,49 @@ func GetMainDict(f_name string) (map[string]interface{}, error) {
 }
 
 //Gets the info dict out of a main dict, returns nil if it doesn't exists
-func GetInfoDict(m map[string]interface{}) map[string]interface{}{
-	for k,v := range m {
+func GetInfoDict(m map[string]interface{}) (map[string]interface{}, error) {
+	for k, v := range m {
 		if k == "info" {
-			return v.(map[string]interface{})
+			return v.(map[string]interface{}), nil
 		}
 	}
-	return nil
+	return nil, errors.New(fmt.Sprint("No info dict found"))
 }
 
 //Gets a string list value from a dictionary (or map[string]Interface{}) that
 //has the key value "key". These reflection things are subtle and recommended
-//reading is 
+//reading is
 //
-//http://blog.golang.org/2011/09/laws-of-reflection.html, 
+//http://blog.golang.org/2011/09/laws-of-reflection.html,
 //http://research.swtch.com/interfaces and the
-//The "fmt" source code. 
+//The "fmt" source code.
 //
-//If there is diffrent list-values in a list you'll have to implement your own 
+//If there is diffrent list-values in a list you'll have to implement your own
 //corresponding function. The string list value is so common that this function is
 //included ;)
 func GetStringListFromDict(key string, dict map[string]interface{}) ([]string, error) {
 
 	iVal, ok := dict[key]
-	
-	
+
 	iList, ok := iVal.([]interface{})
 	if !ok {
-		return nil, errors.New(fmt.Sprint("Not a list value corresponding to the key \"", key,"\" in the dict"))
+		return nil, errors.New(fmt.Sprint("Not a list value corresponding to the key \"", key, "\" in the dict"))
 	}
-	
+
 	list := make([]string, len(iList))
-	for i,it := range iList{
+	for i, it := range iList {
 		v := reflect.ValueOf(it)
-		
+
 		//There's some special conditions for these types of lists and they have
 		//to be tested in the correct order so the program don't panic if a faulty
 		// list-value is corresponding to a key.
 		if v.Kind() == reflect.Slice && v.Index(0).IsValid() && v.Len() == 1 &&
-		 v.Index(0).Elem().Kind() == reflect.String {
+			v.Index(0).Elem().Kind() == reflect.String {
 			list[i] = v.Index(0).Elem().Interface().(string) //Love reflection! xD
 		} else {
-			return nil, errors.New(fmt.Sprint("An incorrect list value corresponding to the key \"", key,"\" in the dict"))
+			return nil, errors.New(fmt.Sprint("An incorrect list value corresponding to the key \"", key, "\" in the dict"))
 		}
 	}
 	return list, nil
-		
+
 }
